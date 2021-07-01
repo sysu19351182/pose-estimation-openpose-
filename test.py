734b3274335ed55acfed96a8437d229b286814c7
sysu19351182 <zhengh75@mail2.sysu.edu.cn>
2021-07-01 14:19:16 +0800

    ADD

---
+++ test.py
@@ -0,0 +1,10 @@
+from zunfantry import zunfanrun_video
+
+prehumans = []
+#vedioname = input("vedioname = ")
+vedioname ="zheng00"
+prehumans = zunfanrun_video(vedioname,prehumans)
+#vedioname = input("vedioname = ")
+vedioname ="zheng01"
+prehumans = zunfanrun_video(vedioname,prehumans)
+
